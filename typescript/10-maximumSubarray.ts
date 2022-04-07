71224b7a6f4ebce11dcfa427b39f7050a32f3a01
儚い〇〇 <96845517+hakanai-sc@users.noreply.github.com>
2022-04-06 17:23:04 -0700

    stub 10

---
+++ typescript/10-maximumSubarray.ts
@@ -0,0 +1,16 @@
+/**
+ * 53. Maximum Subarray
+ * 
+ * Given an integer array nums, find the contiguous subarray (containing at least one 
+ * number) which has the largest sum and return its sum.
+ * 
+ * A subarray is a contiguous part of an array.
+ */
+
+/*
+
+*/
+
+function maxSubArray(nums: number[]): number {
+    // written by hakanai @ https://github.com/hakanai-sc/grind-75
+};
